3eb6b2e3ee0b3317af1d188dbc1d92156f391efb
Leonel Viera <vieraleonel1@gmail.com>
2023-06-23 20:28:04 -0300

    Start building chat page

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -15,40 +15,101 @@ class _ChatPageState extends State<ChatPage> {
     TextEditingController textFieldController = TextEditingController();
 
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Chat Page"),
-          backgroundColor: const Color.fromRGBO(0, 0, 0, 0.5),
-        ),
-        body: Container(
-            color: const Color.fromRGBO(254, 254, 254, 1),
-            padding: const EdgeInsets.only(top: 60, left: 24, right: 24),
-            child: Column(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Container(
+              width: 44,
+              height: 44,
+              margin: const EdgeInsets.only(right: 8),
+              decoration: BoxDecoration(
+                  color: const Color.fromRGBO(242, 248, 255, 1),
+                  borderRadius: BorderRadius.circular(50)),
+              child: const Icon(Icons.android,
+                  size: 24, color: Color.fromRGBO(0, 112, 240, 1)),
+            ),
+            Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const Text("CHAT CHAT CHAT",
+                const Text("Canal de texto",
                     style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.w700,
-                        color: Color.fromRGBO(48, 52, 55, 1))),
-                TextField(
-                  controller: textFieldController,
-                  onSubmitted: (value) {
-                    setState(() {
-                      messages.add(value);
-                    });
-                    textFieldController.clear();
-                  },
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'escribe un mensaje...',
-                  ),
-                ),
-                Expanded(
-                  child: ListView(
-                    children: messages.map((e) => Text(e)).toList(),
-                  ),
+                        color: Color.fromRGBO(32, 35, 37, 1),
+                        fontSize: 14,
+                        fontWeight: FontWeight.w700)),
+                Row(
+                  children: [
+                    Container(
+                        width: 10,
+                        height: 10,
+                        margin: const EdgeInsets.only(right: 8),
+                        decoration: BoxDecoration(
+                            color: const Color.fromRGBO(125, 222, 134, 1),
+                            borderRadius: BorderRadius.circular(50))),
+                    const Text(
+                      "Siempre activo",
+                      style: TextStyle(
+                          color: Color.fromRGBO(114, 119, 122, 1),
+                          fontSize: 14),
+                    ),
+                  ],
                 )
               ],
-            )));
+            ),
+          ],
+        ),
+        backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
+        foregroundColor: Colors.black,
+        elevation: 0,
+      ),
+      body: SafeArea(
+        child: Container(
+          color: const Color.fromRGBO(254, 254, 254, 1),
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: ListView(
+                  children: messages.map((message) => Text(message)).toList(),
+                ),
+              ),
+              Row(children: [
+                Expanded(
+                  child: TextField(
+                    controller: textFieldController,
+                    onSubmitted: (value) {
+                      setState(() {
+                        messages.add(value);
+                      });
+                      textFieldController.clear();
+                    },
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30)),
+                      labelText: 'escribe un mensaje...',
+                    ),
+                  ),
+                ),
+                Container(
+                    margin: const EdgeInsets.only(left: 8),
+                    decoration: BoxDecoration(
+                        color: const Color.fromRGBO(48, 52, 55, 1),
+                        borderRadius: BorderRadius.circular(50)),
+                    child: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          messages.add(textFieldController.text);
+                        });
+                        textFieldController.clear();
+                      },
+                      icon: const Icon(Icons.send),
+                      color: Colors.white,
+                    ))
+              ]),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
